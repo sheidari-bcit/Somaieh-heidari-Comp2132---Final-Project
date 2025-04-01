124ec0dbee65035bf8592968ed121b58c6e4749d
sheidari-bcit <sheidari7@my.bcit.ca>
2025-04-01 10:40:10 -0700

    CHANGE JAVA

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ async function loadWords() {
     words = await response.json();
     startGame();
 }
-//TEST
+//TESTTTT
 function startGame() {
     const randomIndex = Math.floor(Math.random() * words.length);
     selectedWord = words[randomIndex].word.toLowerCase(); 
